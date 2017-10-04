c63f3dea7946863a03eb01230f88cad921dee0d2
Derek <derekisinternet@gmail.com>
2017-10-04 02:58:04 +0000

    added dependency install script

---
+++ install_dependencies.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "Running apt update . . ."
+sudo apt update
+echo "Running apt uprade . . . "
+sudo apt upgrade
+
+echo "Installing dependencies . . . "
+sudo apt install build-essential\
+     gcc cmake git libgtk2.0-dev pkg-config \
+     libavcodec-dev libavformat-dev libswscale-dev \
+     python-dev python-numpy libtbb2 libtbb-dev \
+     libjpeg-dev  libtiff-dev libjasper-dev libdc1394-22-dev
+
+cd $USER_HOME && git clone https://github.com/opencv/opencv.git
+
+cd opencv && mkdir build && cd build
+
+cmake -D CMAKE_BUILD_TYPE=Release ..
