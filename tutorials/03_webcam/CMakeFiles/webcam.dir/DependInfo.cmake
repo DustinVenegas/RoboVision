1294d094d8ea9caba062af160453230af8292cae
Derek <derekisinternet@gmail.com>
2017-10-15 00:23:42 -0700

    hmm

--- tutorials/03_webcam/CMakeFiles/webcam.dir/DependInfo.cmake
+++ tutorials/03_webcam/CMakeFiles/webcam.dir/DependInfo.cmake
@@ -4,14 +4,33 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/home/pi/RoboVision/tutorials/03_webcam/webcam.cpp" "/home/pi/RoboVision/tutorials/03_webcam/CMakeFiles/webcam.dir/webcam.cpp.o"
+  "/Users/Mastermind/C_Shit/RoboVision/tutorials/03_webcam/webcam.cpp" "/Users/Mastermind/C_Shit/RoboVision/tutorials/03_webcam/CMakeFiles/webcam.dir/webcam.cpp.o"
   )
-set(CMAKE_CXX_COMPILER_ID "GNU")
+set(CMAKE_CXX_COMPILER_ID "Clang")
 
 # The include file search paths:
 set(CMAKE_CXX_TARGET_INCLUDE_PATH
-  "/usr/local/include"
-  "/usr/local/include/opencv"
+  "/Users/Mastermind/C_Shit/opencv/build"
+  "/Users/Mastermind/C_Shit/opencv/include"
+  "/Users/Mastermind/C_Shit/opencv/include/opencv"
+  "/Users/Mastermind/C_Shit/opencv/modules/core/include"
+  "/Users/Mastermind/C_Shit/opencv/modules/flann/include"
+  "/Users/Mastermind/C_Shit/opencv/modules/imgproc/include"
+  "/Users/Mastermind/C_Shit/opencv/modules/ml/include"
+  "/Users/Mastermind/C_Shit/opencv/modules/objdetect/include"
+  "/Users/Mastermind/C_Shit/opencv/modules/photo/include"
+  "/Users/Mastermind/C_Shit/opencv/modules/video/include"
+  "/Users/Mastermind/C_Shit/opencv/modules/dnn/include"
+  "/Users/Mastermind/C_Shit/opencv/modules/imgcodecs/include"
+  "/Users/Mastermind/C_Shit/opencv/modules/shape/include"
+  "/Users/Mastermind/C_Shit/opencv/modules/videoio/include"
+  "/Users/Mastermind/C_Shit/opencv/modules/highgui/include"
+  "/Users/Mastermind/C_Shit/opencv/modules/superres/include"
+  "/Users/Mastermind/C_Shit/opencv/modules/ts/include"
+  "/Users/Mastermind/C_Shit/opencv/modules/features2d/include"
+  "/Users/Mastermind/C_Shit/opencv/modules/calib3d/include"
+  "/Users/Mastermind/C_Shit/opencv/modules/stitching/include"
+  "/Users/Mastermind/C_Shit/opencv/modules/videostab/include"
   )
 
 # Targets to which this target links.
--- tutorials/03_webcam/cmake_install.cmake
+++ tutorials/03_webcam/cmake_install.cmake
@@ -1,8 +1,8 @@
-# Install script for directory: /home/pi/RoboVision/tutorials/03_webcam
+# Install script for directory: /Users/Mastermind/C_Shit/RoboVision/tutorials/03_webcam
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "/usr/local")
+  set(CMAKE_INSTALL_PREFIX "/Users/Mastermind/C_Shit/opencv")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -27,11 +27,6 @@ if(NOT CMAKE_INSTALL_COMPONENT)
   endif()
 endif()
 
-# Install shared libraries without execute permission?
-if(NOT DEFINED CMAKE_INSTALL_SO_NO_EXE)
-  set(CMAKE_INSTALL_SO_NO_EXE "1")
-endif()
-
 if(CMAKE_INSTALL_COMPONENT)
   set(CMAKE_INSTALL_MANIFEST "install_manifest_${CMAKE_INSTALL_COMPONENT}.txt")
 else()
@@ -40,5 +35,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/pi/RoboVision/tutorials/03_webcam/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/Users/Mastermind/C_Shit/RoboVision/tutorials/03_webcam/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
