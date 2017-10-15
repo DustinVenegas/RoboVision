4a38abb9c2d275d5990e6b14dcb4a020511df120
Derek <derekisinternet@gmail.com>
2017-10-15 01:40:47 -0700

    more refactoring

--- makeCMakeLists.sh
+++ makeCMakeLists.sh
@@ -5,8 +5,10 @@ if [ $# -eq 0 ]
         echo 'No Project name specified'
     else
         file=CMakeLists.txt
+        opencv_location=$(echo $HOME/opencv)
         printf 'cmake_minimum_required(VERSION 2.8)\n' >> $file
         printf "project( $1 )\n" >> $file
+        printf "set(OpenCV_DIR $opencv_location)\n" >> $file
         printf 'find_package( OpenCV REQUIRED )\n' >> $file
         printf "add_executable( $1 src/$1.cpp )\n" >> $file
         printf "add_definitions(-std=c++0x)\n" >> $file
